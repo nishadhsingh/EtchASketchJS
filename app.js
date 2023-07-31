d342e9eb3eb83bc3b0b890a0ed231119ae78f79a
Nishadh <nishadhsingh@gmail.com>
2023-07-31 14:58:50 +0530

    replaced flexBox with grid

--- app.js
+++ app.js
@@ -2,40 +2,42 @@ let dataMap = new Map();
 
 const container = document.querySelector(".container");
 const rect = container.getBoundingClientRect();
-console.log({container,rect});
+const DEFAULT_SIZE = 500;
+console.log({ container, rect });
 
 let boxProperties = new Map();
 
 
 
-dataMap.set("WIDTH",Math.trunc(rect.width));
-dataMap.set("HEIGHT",Math.trunc(rect.height));
+dataMap.set("WIDTH", Math.trunc(rect.width));
+dataMap.set("HEIGHT", Math.trunc(rect.height));
 
 
 let boxCount = 1;
 let boxWidth = 20;
 let boxHeight = 20;
 let containerPadding = "10vh";
-let N_side = 16;
-let Square_count = N_side*N_side;
+let N_side = 100;
+let Square_count = N_side * N_side;
+let containerDimention = 500;
+let boxDimention = Math.trunc(500 / N_side);
 
-console.log(dataMap);
+console.log({ boxDimention });
 
-function drawGrid()
-{
-    for(let i = 1;i<=Square_count;i++)
-    {
-        drawBox(boxWidth,boxHeight);
-        
+function drawcontainer() {
+    container.style.gridTemplateColumns = `repeat(${N_side}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${N_side}, 1fr)`
+    container.style.height = DEFAULT_SIZE+"px";
+    container.style.width = DEFAULT_SIZE+"px";
+
+    for (let i = 0; i < N_side * N_side; i++) {
+        drawBox()
     }
-    container.style.width = N_side*boxWidth+"px";
-    container.style.height = N_side*boxHeight+"px";
-    console.log({container});
+
 }
-function upDateColor(event)
-{
+function upDateColor(event) {
     let box = event.originalTarget;
-    box.style.backgroundColor =  "#000000";
+    box.style.backgroundColor = "#000000";
 }
 
 
@@ -43,17 +45,14 @@ function upDateColor(event)
 
 
 
-function drawBox(width,height)
-{
-    let ele =  document.createElement("div");
-    ele.style.width = width+"px";
-    ele.style.height = height+"px";
+function drawBox() {
+    let ele = document.createElement("div");
     ele.classList.add("box");
-    ele.addEventListener("pointerover",upDateColor);
-    ele.addEventListener("touchmove",upDateColor);
+    ele.addEventListener("pointerover", upDateColor);
+    ele.addEventListener("touchmove", upDateColor);
     container.appendChild(ele);
-    
+
 }
 
 
-drawGrid();
+drawcontainer();
